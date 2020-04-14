3cb043f1c4a67c4a939e0832cbcf4f087c67cef1
Justin Calvo <justin_calvo@hotmail.com>
2020-04-14 16:29:21 -0400

    trying without the pool connect added earlier

--- server/PostgreSQL/DB/index.js
+++ server/PostgreSQL/DB/index.js
@@ -2,17 +2,17 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'power_user',
   password: 'justin',
-  host: 'localhost',
+  host: '18.191.183.167',
   database: 'airbnb',
   port: 5432,
 });
 
-pool.connect((err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Connected');
-  }
-});
+// pool.connect((err) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log('Connected');
+//   }
+// });
 
 module.exports = pool;
